5de1801b89032e266be1f0825964dba7064f3f6f
Jean-Pierre Fortune <jean-pierre.fortune@ialto.com>
2025-02-25 14:05:36 +0100

    ci: update to Dokka 2.0.0 and add maven publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,16 @@
 plugins {
     id("com.diffplug.spotless") version "6.25.0"
-    id("org.jetbrains.dokka") version "1.9.20"
+    id("org.jetbrains.dokka") version "2.0.0"
 }
 
 buildscript {
     dependencies {
         classpath("javax.xml.bind:jaxb-api:2.3.1")
         classpath("com.sun.xml.bind:jaxb-core:2.3.0.1")
-        classpath("com.sun.xml.bind:jaxb-impl:2.3.1")
+        classpath("com.sun.xml.bind:jaxb-impl:2.3.9")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.22")
         classpath("com.android.tools.build:gradle:8.8.1")
-        classpath("org.eclipse.keyple:keyple-gradle:0.2.+") { isChanging = true }
+        classpath("org.jetbrains.dokka:dokka-base:2.0.0")
     }
 
     repositories {
